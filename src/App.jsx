d9317dde6fc920bfbca7fc4063138ffd295bd995
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2023-10-17 22:12:10 +1100

    Track user as array of indices instaead of a whole board

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,33 @@ import {
   combineFields,
 } from "./tetris";
 const board = generateTetrisField(10, 20);
-const userBoard = generateEmptyField(10, 20);
-userBoard[26] = "cyan";
-userBoard[38] = "cyan";
-userBoard[50] = "cyan";
-userBoard[62] = "cyan";
+// const userBoard = generateEmptyField(10, 20);
+// userBoard[26] = "cyan";
+// userBoard[38] = "cyan";
+// userBoard[50] = "cyan";
+// userBoard[62] = "cyan";
+const queue = generateQueue(100000);
+//const currentShape = queue.shift();
+const currentShape = "straight";
+const location = [28, 29, 30, 31];
 const initialState = {
   board,
-  userBoard,
-  queue: generateQueue(100000),
+  queue,
+  currentShape,
+  location,
 };
 console.log(initialState);
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const userBoard = generateEmptyField(10, 20);
+  for (let i = 0; i < state.location.length; i++) {
+    userBoard[state.location[i]] = "cyan";
+  }
   return (
     <TetrisContext.Provider value={{ state, dispatch }}>
       <div className="flex gap-6 flex-wrap bg-black">
-        <Field bricks={combineFields(state.board, state.userBoard)} />
+        <Field bricks={combineFields(state.board, userBoard)} />
       </div>
     </TetrisContext.Provider>
   );
